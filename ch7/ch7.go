cb95e5b5bcf8764015e7abeee14ba6baf9315c44
Liupeng <liupenglily@163.com>
2020-05-07 09:12:17 +0800

    ch7 sort.Interface排序

--- ch7/ch7.go
+++ ch7/ch7.go
@@ -5,13 +5,17 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"os"
+	"sort"
+	"text/tabwriter"
 	"time"
 )
 
 // Ch7 第七章入口函数
 func Ch7() {
 	// sleep()
-	newParam()
+	// newParam()
+	chSort()
 }
 
 // 空接口，空接口可以接收任何类型
@@ -77,4 +81,93 @@ func newParam() {
 	fmt.Printf("%T\n", a)
 	var b io.Writer
 	fmt.Printf("%T\n", b)
+}
+
+// 7.6使用sort.interface来排序
+
+// Track 播放列表
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+var tracks = []*Track{
+	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+	{"Go", "Moby", "Moby", 1992, length("3m37s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+func printTracks(tracks []*Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, val := range tracks {
+		fmt.Fprintf(tw, format, val.Title, val.Artist, val.Album, val.Year, val.Length)
+	}
+	tw.Flush()
+}
+
+// 按艺术家排序
+type byArtist []*Track
+
+func (x byArtist) Len() int {
+	return len(x)
+}
+
+func (x byArtist) Less(i, j int) bool {
+	return x[i].Artist < x[j].Artist
+}
+
+func (x byArtist) Swap(i, j int) {
+	x[i], x[j] = x[j], x[i]
+}
+
+// 多维度排序
+
+type customSort struct {
+	t    []*Track
+	less func(x, y *Track) bool
+}
+
+func (x customSort) Len() int {
+	return len(x.t)
+}
+
+func (x customSort) Less(i, j int) bool {
+	return x.less(x.t[i], x.t[j])
+}
+
+func (x customSort) Swap(i, j int) {
+	x.t[i], x.t[j] = x.t[j], x.t[i]
+}
+
+func chSort() {
+	sort.Sort(byArtist(tracks))
+	printTracks(tracks)
+
+	sort.Sort(customSort{tracks, func(x, y *Track) bool {
+		if x.Title != y.Title {
+			return x.Title < y.Title
+		}
+		if x.Year != y.Year {
+			return x.Year < y.Year
+		}
+		if x.Length != y.Length {
+			return x.Length < y.Length
+		}
+		return false
+	}})
+	printTracks(tracks)
 }
