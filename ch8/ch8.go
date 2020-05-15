511df6882e4c64b5648315787030cc2a65d729b0
Liupeng <liupenglily@163.com>
2020-05-15 13:39:41 +0800

    ch8

--- ch8/ch8.go
+++ ch8/ch8.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"net/http"
 	"os"
 	"strings"
 	"sync"
 	"time"
+
+	"golang.org/x/net/html"
 )
 
 // Ch8 第八章入口函数
@@ -17,7 +20,8 @@ func Ch8() {
 	// clock()
 	// netcat2()
 	// pipe()
-	Noname()
+	// Noname()
+	ch8Mul()
 }
 
 func spinner(delay time.Duration) {
@@ -184,4 +188,91 @@ func Noname() {
 	for v := range ch {
 		fmt.Println(v)
 	}
+}
+
+// Extract makes an HTTP GET request to the specified URL, parses
+// the response as HTML, and returns the links in the HTML document.
+func extract(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	var links []string
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue // ignore bad URLs
+				}
+				links = append(links, link.String())
+			}
+		}
+	}
+	forEachNode(doc, visitNode, nil)
+	return links, nil
+}
+
+// Copied from gopl.io/ch5/outline2.
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
+
+// 计数信号量，最多创建20个goroutine
+var token = make(chan struct{}, 20)
+
+func crawl(url string) []string {
+	list := []string{}
+	fmt.Println(url)
+	// struct{}是数据类型，struct{}{}是结构体类型空值
+	token <- struct{}{}
+	list, _ = extract(url)
+	<-token
+	return list
+}
+func ch8Mul() {
+	worklist := make(chan []string)
+	var n int
+	n++
+	// 匿名函数
+	go func() {
+		worklist <- os.Args[1:]
+	}()
+
+	seen := make(map[string]bool)
+	for ; n > 0; n-- {
+		lists := <-worklist
+		for _, link := range lists {
+			if !seen[link] {
+				seen[link] = true
+				n++
+				go func(link string) {
+					worklist <- crawl(link)
+				}(link)
+			}
+		}
+	}
+	fmt.Printf("n=%v", n)
 }
