789eec18d5eb2b6389581702f30a30c3297a9579
Liupeng <liupenglily@163.com>
2020-05-20 11:01:40 +0800

    ch9

--- ch9/ch9.go
+++ ch9/ch9.go
@@ -86,25 +86,98 @@ type result struct {
 // Memo 缓存结构
 type Memo struct {
 	f     Func
+	mux   sync.Mutex
 	cache map[string]result
 }
 
 func new(f Func) *Memo {
 	return &Memo{
 		f:     f,
+		mux:   sync.Mutex{},
 		cache: make(map[string]result),
 	}
 }
 
+// get 加了信号量保护,完全串行执行，性能很差,下面为输出
+// command-line-arguments
+// https://golang.google.cn/pkg/, 437.9196ms, 57808 bytes
+// https://golang.google.cn/doc/, 656.2899ms, 9008 bytes
+// https://golang.google.cn/pkg/archive/, 875.4551ms, 5453 bytes
+// https://golang.google.cn/pkg/, 875.4551ms, 57808 bytes
+// https://golang.google.cn/pkg/archive/, 875.4551ms, 5453 bytes
+// https://golang.google.cn/doc/, 875.4551ms, 9008 bytes
+// func (memo *Memo) get(key string) (interface{}, error) {
+// 	memo.mux.Lock()
+// 	res, ok := memo.cache[key]
+// 	if !ok {
+// 		res.value, res.err = memo.f(key)
+// 		memo.cache[key] = res
+// 	}
+// 	memo.mux.Unlock()
+// 	return res.value, res.err
+// }
+
+// 第二个版本
+// https://golang.google.cn/pkg/archive/, 323.306ms, 5453 bytes
+// https://golang.google.cn/pkg/archive/, 323.306ms, 5453 bytes
+// https://golang.google.cn/doc/, 324.3431ms, 9008 bytes
+// https://golang.google.cn/doc/, 330.2877ms, 9008 bytes
+// https://golang.google.cn/pkg/, 333.3073ms, 57808 bytes
+// https://golang.google.cn/pkg/, 334.3063ms, 57808 bytes
 func (memo *Memo) get(key string) (interface{}, error) {
+	memo.mux.Lock()
 	res, ok := memo.cache[key]
+	memo.mux.Unlock()
 	if !ok {
 		res.value, res.err = memo.f(key)
+		memo.mux.Lock()
 		memo.cache[key] = res
+		memo.mux.Unlock()
 	}
 	return res.value, res.err
 }
 
+func (memo *Memo1) get(key string) (value interface{}, err error) {
+	memo.mu.Lock()
+	e := memo.cache[key]
+	if e == nil {
+		// This is the first request for this key.
+		// This goroutine becomes responsible for computing
+		// the value and broadcasting the ready condition.
+		e = &entry{ready: make(chan struct{})}
+		memo.cache[key] = e
+		memo.mu.Unlock()
+		fmt.Println(key)
+		e.res.value, e.res.err = memo.f(key)
+
+		close(e.ready) // broadcast ready condition
+	} else {
+		// This is a repeat request for this key.
+		memo.mu.Unlock()
+
+		<-e.ready // wait for ready condition
+	}
+	return e.res.value, e.res.err
+}
+
+// Memo1 升级版
+type Memo1 struct {
+	f     Func
+	mu    sync.Mutex // guards cache
+	cache map[string]*entry
+}
+type entry struct {
+	res   result
+	ready chan struct{}
+}
+
+func new1(f Func) *Memo1 {
+	return &Memo1{
+		f:     f,
+		mu:    sync.Mutex{},
+		cache: make(map[string]*entry),
+	}
+}
 func httpGetBody(url string) (interface{}, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -137,7 +210,7 @@ func ch9Memo() {
 
 // 并行执行版本1
 func ch9Memo1() {
-	m := new(httpGetBody)
+	m := new1(httpGetBody)
 	var wg sync.WaitGroup
 	urls := []string{
 		"https://golang.google.cn/pkg/",
@@ -156,7 +229,7 @@ func ch9Memo1() {
 				fmt.Println(err)
 			}
 			fmt.Printf("%s, %s, %d bytes\n", url, time.Since(start), len(value.([]byte)))
-			wg.Done()
+			defer wg.Done()
 		}(url)
 	}
 	wg.Wait()
